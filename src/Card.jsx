e20b5054e58a682c05827c915d884c21b4548f7b
Omar <rajamuhammedomar@gmail.com>
2023-10-01 06:09:23 -0600

    remove comment

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./Card.css";
 
 export default function Card(props) {
-  // const [id, setId] = useState(0)
   return (
     <div className="card" onClick={(e) => props.onClick(e)}>
       {props.id}
